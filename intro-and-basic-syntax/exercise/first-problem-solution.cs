1ff3fc33ea665c9187b3fd52b6093b6cacee2335
Kristiyan <kris.ivanov95@outlook.com>
2018-03-08 11:09:55 +0200

    Added file with solution for the first problem of the first exercise from Programming Fundamentals

---
+++ intro-and-basic-syntax/exercise/first-problem-solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+   
+            static void Main()
+            {
+                var first = int.Parse(Console.ReadLine());
+                var second = int.Parse(Console.ReadLine());
+                var third = int.Parse(Console.ReadLine());
+                var forth = int.Parse(Console.ReadLine());
+
+                var firstmod = first.ToString("D4");
+                var secondmod = second.ToString("D4");
+                var thirdmod = third.ToString("D4");
+                var forthmod = forth.ToString("D4");
+
+            Console.WriteLine($"{firstmod} {secondmod} {thirdmod} {forthmod}");
+
+
+
+        }
+    }
+
+}
