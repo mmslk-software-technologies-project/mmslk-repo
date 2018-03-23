34751d9f1723ed7c2788a8b0a9d9669a1eb08a32
Kristiyan <kris.ivanov95@outlook.com>
2018-03-24 01:08:05 +0200

    third and fifth

---
+++ intro-and-basic-syntax/exercise/fifth-problem-solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace xcvbnm
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+
+            int currHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+
+            int currEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Health: |{0}{1}|",new string('|', currHealth), new string('.', maxHealth));
+            Console.WriteLine("Energy: |{0}{1}|",new string('|', currEnergy), new string('.', maxEnergy));
+        }
+    }
+}
---
+++ intro-and-basic-syntax/exercise/third-problem-solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace xcvbnm
+{
+    class Program
+    {
+        static void Main()
+        {
+            double miles = double.Parse(Console.ReadLine());
+
+            double kilometerPerMile = 1.60934;
+
+            double kilometer = miles * kilometerPerMile;
+
+            Console.WriteLine($"{kilometer:F2}");
+        }
+    }
+}
+
+
