1c067233774ba1cb9d01b558ee76b3420bd4281f
jollybg <marin.tsankov@gmail.com>
2018-03-23 23:17:12 +0200

    Added solution for the fourth problem

---
+++ intro-and-basic-syntax/exercise/fourth-problem-solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P04_BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugarConetent = int.Parse(Console.ReadLine());
+
+            double energy = volume * energyContent / 100.0;
+            double sugar = volume * sugarConetent / 100.0;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+        }
+    }
+}
