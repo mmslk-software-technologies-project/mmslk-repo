c0745718f5fc45a8ea9086100037b17dfc0309f7
MarinTsankov <marin.tsankov@gmail.com>
2018-03-23 15:15:02 +0200

    Added solution for the second problem

---
+++ intro-and-basic-syntax/exercise/second-problem-solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P02_RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            double area = sideA * sideB;
+
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
